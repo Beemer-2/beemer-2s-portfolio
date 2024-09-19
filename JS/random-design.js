778f7ced5c2a09428fd5becc542b07f6a60a95c5
Beemer-2 <151047145+Beemer-2@users.noreply.github.com>
2024-09-19 11:38:09 +0100

    Made the "go to a random design" link hideable with a button press and put it on all sites.

--- JS/random-design.js
+++ JS/random-design.js
@@ -1,7 +1,20 @@
 "strict mode"
 
-let randomDesignLinks = ["design-1.html", "design-2.html"]
+let randomDesignLinks = ["design-1.html", "design-2.html"];
 
-document.getElementById("random-site-a").addEventListener("click", () => {
-    document.getElementById("random-site-a").href = randomDesignLinks[Math.floor(Math.random() * randomDesignLinks.length)];
-})
+let randomSiteA = document.getElementById("random-site-a");
+
+randomSiteA.addEventListener("click", () => {
+    randomSiteA.href = randomDesignLinks[Math.floor(Math.random() * randomDesignLinks.length)];
+});
+
+document.addEventListener("keydown", (q) => {
+    if (q.key == "q") {
+        let opacityCheck = window.getComputedStyle(randomSiteA).getPropertyValue("opacity");
+        if (opacityCheck == 1) {
+            randomSiteA.style.opacity = 0;
+        } else {
+            randomSiteA.style.opacity = 1;
+        }
+    }
+});
