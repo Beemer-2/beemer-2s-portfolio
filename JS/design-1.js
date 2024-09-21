3654c9de1d2459d86fd6098f07c3388649ed89e8
Beemer-2 <151047145+Beemer-2@users.noreply.github.com>
2024-09-21 11:37:56 +0100

    Added some site animations.

---
+++ JS/design-1.js
@@ -0,0 +1,34 @@
+"strict mode"
+
+document.getElementById("main-paragraph-h2").classList.add("hidden-main");
+document.getElementById("main-paragraph-p").classList.add("hidden-main");
+document.getElementById("main-paragraph-ul").classList.remove("side-show");
+document.getElementById("main-paragraph-ul").classList.add("hidden-side");
+
+console.log("check");
+
+let clientHeightLet = document.documentElement.clientHeight;
+
+
+
+let screenSize = document.body.getBoundingClientRect();
+console.log(screenSize, screenSize.width, screenSize.height, screenSize.top, screenSize.bottom);
+document.addEventListener("scroll", () => {
+    screenSize = document.body.getBoundingClientRect();
+    clientHeightLet = document.documentElement.clientHeight;
+    if (clientHeightLet > document.getElementById("main-paragraph-h2").getBoundingClientRect().y + document.getElementById("main-paragraph-h2").getBoundingClientRect().height / 5) {
+        console.log("e");
+        document.getElementById("main-paragraph-h2").classList.add("main-show");
+
+    } 
+    
+    if (clientHeightLet > document.getElementById("main-paragraph-p").getBoundingClientRect().y + document.getElementById("main-paragraph-p").getBoundingClientRect().height / 5) {
+        document.getElementById("main-paragraph-p").classList.add("main-show");
+    }
+
+    if (clientHeightLet > document.getElementById("main-paragraph-ul").getBoundingClientRect().y + document.getElementById("main-paragraph-ul").getBoundingClientRect().height / 5) {
+        document.getElementById("main-paragraph-ul").classList.add("side-show");
+    }
+    //console.log(screenSize, screenSize.width, screenSize.height, screenSize.top, screenSize.bottom)
+});
+
