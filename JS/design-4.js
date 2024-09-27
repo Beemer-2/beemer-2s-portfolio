d52201e5b51576fb57d1c95f1cc906a9343dfbb5
Beemer-2 <151047145+Beemer-2@users.noreply.github.com>
2024-09-27 21:27:34 +0100

    Added icons and fixed UI

--- JS/design-4.js
+++ JS/design-4.js
@@ -44,16 +44,46 @@ document.addEventListener("click", (e) => {
     rightClickMenu.style.display = "none";
 });
 
+
+let currentNewFolderCount = 5;
+let currentTextDocCount = 0;
+
 document.getElementById("new-folder").addEventListener("click", (e) => {
+
+    currentNewFolderCount += 1;
+    let newFolderString = `New Folder (${currentNewFolderCount-1})`;
+
+    let newContainer = document.createElement("div");
+    newContainer.id = newFolderString;
+    document.getElementById("desktop").appendChild(newContainer);
+
     let newFolder = document.createElement("img");
-    newFolder.src = "../ASSETS/placeholder-image.png";
-    document.getElementById("desktop").appendChild(newFolder);
+    newFolder.src = "../ASSETS/Folder-Icon.png";
+    document.getElementById(newFolderString).appendChild(newFolder);
+
+    let newFolderName = document.createElement("p");
+    newFolderName.innerHTML = newFolderString;
+    newFolderName.classList.add("desktop-item-p");
+    document.getElementById(newFolderString).appendChild(newFolderName);
+
 });
 
 document.getElementById("new-text-document").addEventListener("click", (e) => {
-    let newTextDoc = document.createElement("img");
-    newTextDoc.src = "../ASSETS/placeholder-text-document.png";
-    document.getElementById("desktop").appendChild(newTextDoc);
+    currentTextDocCount += 1;
+    let newDocString = `New Text Document (${currentTextDocCount-1})`;
+
+    let newContainer = document.createElement("div");
+    newContainer.id = newDocString;
+    document.getElementById("desktop").appendChild(newContainer);
+
+    let newDoc = document.createElement("img");
+    newDoc.src = "../ASSETS/Text-doc.png";
+    document.getElementById(newDocString).appendChild(newDoc);
+
+    let newDocName = document.createElement("p");
+    newDocName.innerHTML = newDocString;
+    newDocName.classList.add("desktop-item-p");
+    document.getElementById(newDocString).appendChild(newDocName);
 });
 
 
@@ -61,3 +91,7 @@ document.getElementById("new-text-document").addEventListener("click", (e) => {
 function fullscreen() {
     console.log(document.body.requestFullscreen());
 }
+
+
+
+
